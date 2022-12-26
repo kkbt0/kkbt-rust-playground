888b68b02146a37be8630985c69c138e36b444ad
kkbt <0@ftls.xyz>
2022-12-26 16:44:59 +0800

    lc: 1759

--- entry/src/main.rs
+++ entry/src/main.rs
@@ -1,17 +1,3 @@
-use kkbt::SKV;
 fn main() {
-    // SKV::init("data/index", "data/data").unwrap();
-
-    let skv = SKV::config("data/index", "data/data");
-
-    skv.set("kkbt", Vec::from("我是恐咖兵糖1".as_bytes()));
-    skv.set("kkbt", Vec::from("".as_bytes()));
-    assert_eq!(skv.get_utf8("kkbt"),None);
-
-    skv.set("kkbt", Vec::from("我是恐咖兵糖3".as_bytes()));
-    assert_eq!(skv.get_utf8("kkbt"),Some("我是恐咖兵糖3".to_string()));
-
-    assert_eq!(skv.get_utf8("abcdefg"),None);
-
-   // kkbt::show_file_hex("data/index", 3).unwrap();
+    println!("Hello, world!");
 }
--- leetcode/src/lib.rs
+++ leetcode/src/lib.rs
@@ -8,30 +8,46 @@ mod qa {
         // https://leetcode.cn/problems/count-number-of-homogenous-substrings/
         // 1759. 统计同构子字符串的数目
         pub fn count_homogenous(s: String) -> i32 {
-            let mut ans:i64 = 0;
+            let mut ans: i64 = 0;
             let mut count = 0;
             let mut last_char = s.chars().last().unwrap();
             for c in s.chars() {
                 if c == last_char {
                     count += 1;
                 } else {
-                    ans += count*(count + 1)/2;
+                    ans += count * (count + 1) / 2;
                     last_char = c;
                     count = 1;
                 }
             }
-            ans += count*(count + 1)/2; 
-            (ans%1000_000_007) as i32
+            ans += count * (count + 1) / 2;
+            (ans % 1000_000_007) as i32
+        }
+        // https://leetcode.cn/problems/count-number-of-homogenous-substrings/solution/by-kyushu-e5bb/
+        pub fn count_homogenous2(s: String) -> i32 {
+            let x = s.as_bytes().windows(2);
+            println!("{:?}",x);
+            s.as_bytes()
+                .windows(2)
+                .fold((1, 1), |(cnt, ret), ch| {
+                    if ch[0] == ch[1] {
+                        (cnt + 1, (ret + cnt + 1) % 1_000_000_007)
+                    } else {
+                        (1, (ret + 1) % 1_000_000_007)
+                    }
+                })
+                .1
         }
     }
+
     #[test]
     fn test_count_homogenous() {
-        // let str = String::from("zzzzz"); // 5 4 3 2 1 // (num+1)*num/2 
-        assert_eq!(Solution::count_homogenous(String::from("zzzzz")),15);
-        assert_eq!(Solution::count_homogenous(String::from("abbcccaa")),13); // a bb ccc aaa
-        assert_eq!(Solution::count_homogenous(String::from("xy")),2);
+        // let str = String::from("zzzzz"); // 5 4 3 2 1 // (num+1)*num/2
+        assert_eq!(Solution::count_homogenous(String::from("zzzzz")), 15);
+        assert_eq!(Solution::count_homogenous(String::from("abbcccaa")), 13); // a bb ccc aaa
+        assert_eq!(Solution::count_homogenous2(String::from("xy")), 2);
     }
-    // Test Solution 
+    // Test Solution
     fn test_add() -> i32 {
         1 + 1
     }
@@ -40,4 +56,3 @@ mod qa {
         assert_eq!(1 + 1, test_add());
     }
 }
-
