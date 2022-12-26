9f18be1d855afcf24f28a0e05af5d9ba1503314e
kkbt <0@ftls.xyz>
2022-12-26 11:44:23 +0800

    feat: skv

--- entry/src/main.rs
+++ entry/src/main.rs
@@ -1,29 +1,17 @@
+use kkbt::SKV;
 fn main() {
-    println!("hello");
-    kkbt::show_file_hex("data/1",999).unwrap();
-   // SKV::init("1", "2").unwrap();
+    // SKV::init("data/index", "data/data").unwrap();
+
     let skv = SKV::config("data/index", "data/data");
-    dbg!(skv.get("k1").unwrap());
-}
-struct SKV<'a> {
-    index_file: &'a str,
-    data_file: &'a str,
-}
-impl<'a> SKV<'a> {
-    // create two files
-    #[allow(dead_code)]
-    fn init(index_file:&'a str,data_file:&'a str) -> std::io::Result<SKV<'a>> {
-        std::fs::File::create(index_file)?;
-        std::fs::File::create(data_file)?;
-        Ok(Self { index_file,data_file })
-    }
-    fn config(index_file:&'a str,data_file:&'a str) -> SKV<'a> {
-        Self { index_file,data_file }
-    }
-    fn get(self,key:&'a str) -> std::io::Result<String> {
-       // let file = std::fs::File::open(self.index_file)?;
-        println!("{}",key);
-        let str1 = std::fs::read_to_string(self.data_file)?;
-        Ok(str1)
-    }
+
+    skv.set("kkbt", Vec::from("我是恐咖兵糖1".as_bytes()));
+    skv.set("kkbt", Vec::from("".as_bytes()));
+    assert_eq!(skv.get_utf8("kkbt"),None);
+
+    skv.set("kkbt", Vec::from("我是恐咖兵糖3".as_bytes()));
+    assert_eq!(skv.get_utf8("kkbt"),Some("我是恐咖兵糖3".to_string()));
+
+    assert_eq!(skv.get_utf8("abcdefg"),None);
+
+   // kkbt::show_file_hex("data/index", 3).unwrap();
 }
