8573d322aaa9da35069aa97ca8d580d9f0de9ba2
kkbt <0@ftls.xyz>
2022-12-26 09:04:58 +0800

    feat: skv init

--- entry/src/main.rs
+++ entry/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    println!("hello");
+    kkbt::show_file_hex("data/1",999).unwrap();
+   // SKV::init("1", "2").unwrap();
+    let skv = SKV::config("data/index", "data/data");
+    dbg!(skv.get("k1").unwrap());
+}
+struct SKV<'a> {
+    index_file: &'a str,
+    data_file: &'a str,
+}
+impl<'a> SKV<'a> {
+    // create two files
+    #[allow(dead_code)]
+    fn init(index_file:&'a str,data_file:&'a str) -> std::io::Result<SKV<'a>> {
+        std::fs::File::create(index_file)?;
+        std::fs::File::create(data_file)?;
+        Ok(Self { index_file,data_file })
+    }
+    fn config(index_file:&'a str,data_file:&'a str) -> SKV<'a> {
+        Self { index_file,data_file }
+    }
+    fn get(self,key:&'a str) -> std::io::Result<String> {
+       // let file = std::fs::File::open(self.index_file)?;
+        println!("{}",key);
+        let str1 = std::fs::read_to_string(self.data_file)?;
+        Ok(str1)
+    }
 }
