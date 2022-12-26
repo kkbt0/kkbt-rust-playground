4bc91083923a1b4edb7d62d753ece5529f2bfd3c
kkbt <0@ftls.xyz>
2022-12-26 08:51:39 +0800

    lc: add 1759

---
+++ leetcode/src/lib.rs
@@ -0,0 +1,43 @@
+#[allow(dead_code)]
+mod qa {
+    fn red(msg: &str) {
+        println!("\x1b[91m{}\x1b[0m", msg);
+    }
+    struct Solution;
+    // https://leetcode.cn/problems/count-number-of-homogenous-substrings/
+    // 1759. 统计同构子字符串的数目
+    impl Solution {
+        pub fn count_homogenous(s: String) -> i32 {
+            let mut ans:i64 = 0;
+            let mut count = 0;
+            let mut last_char = s.chars().last().unwrap();
+            for c in s.chars() {
+                if c == last_char {
+                    count += 1;
+                } else {
+                    ans += count*(count + 1)/2;
+                    last_char = c;
+                    count = 1;
+                }
+            }
+            ans += count*(count + 1)/2; 
+            (ans%1000_000_007) as i32
+        }
+    }
+    #[test]
+    fn test_count_homogenous() {
+        // let str = String::from("zzzzz"); // 5 4 3 2 1 // (num+1)*num/2 
+        assert_eq!(Solution::count_homogenous(String::from("zzzzz")),15);
+        assert_eq!(Solution::count_homogenous(String::from("abbcccaa")),13); // a bb ccc aaa
+        assert_eq!(Solution::count_homogenous(String::from("xy")),2);
+    }
+    // Test Solution 
+    fn test_add() -> i32 {
+        1 + 1
+    }
+    #[test]
+    fn test() {
+        assert_eq!(1 + 1, test_add());
+    }
+}
+
