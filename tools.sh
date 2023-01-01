e28d406614553842a6030fbbf43b39d27ee8cfa6
kkbt <0@ftls.xyz>
2023-01-01 16:14:41 +0800

    doc

--- tools.sh
+++ tools.sh
@@ -2,15 +2,15 @@
 
 cd `dirname $0`
 case $1 in
-    # cargo doc 生成
+    # cargo doc 生成 for gitee
     doc)
         cargo doc
-        echo "<meta http-equiv='refresh' content='0; url=/leetcode'>" > target/doc/index.html 
+        echo "<meta http-equiv='refresh' content='0; url=/kkbt-rust-playground/leetcode'>" > target/doc/index.html 
         ;;
-    # cargo doc 生成 并 运行
+    # cargo doc 生成 并 运行 for 本地
     docr)
         cargo doc
-        echo "<meta http-equiv='refresh' content='0; url=/leetcode'>" > target/doc/index.html 
+        echo "<meta http-equiv='refresh' content='0; url=/kkbt-rust-playground/leetcode'>" > target/doc/index.html 
         # --- for Windows WSL2 Begin ---
         explorer.exe file://wsl.localhost/Ubuntu/home/kkbt/PlayGround/target/doc/leetcode/index.html
         # --- for Windows WSL2 End ---
